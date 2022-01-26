3b48e18e56fc29e03233ab6b0002e8525ac7e036
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-01-27 01:55:52 +0400

    Time: 936 ms (61.86%), Space: 22.8 MB (14.12%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_so_far = prices[0]
+        maxx = 0
+        for i in range(1, len(prices)):
+            price = prices[i]
+            min_so_far = min(min_so_far, price)
+            maxx = max(maxx, price - min_so_far)
+
+        return maxx
+        
