5fa2720820a74dcc6000f46bca7c0d5d4f67bbe3
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-01-29 21:07:08 +0400

    Time: 173 ms (22.65%), Space: 18.4 MB (5.23%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,11 +1,24 @@
 class Solution(object):
+    def rec(self, n):
+        if n in self.dp:
+            return self.dp[n]
+        if n <= 1:
+            return n
+
+        count = self.rec(n >> 1)
+        if n % 2 == 1:
+            count += 1
+
+        self.dp[n] = count
+        return count
+
     def countBits(self, n):
         """
         :type n: int
         :rtype: List[int]
         """
-        self
+        self.dp = {}
         ans = []
-        for i in range(n+1):
-            ans.append((bin(i)[2::]).count('1'))
+        for i in range(n + 1):
+            ans.append(self.rec(i))
         return ans
