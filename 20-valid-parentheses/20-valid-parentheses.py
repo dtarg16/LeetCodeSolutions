94429ad80d9be38bcecc02cb4e838273a27dde4d
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-01-31 01:05:46 +0400

    Time: 19 ms (77.58%), Space: 13.8 MB (13.26%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        left = ['(', '{', '[']
+        right = [')', '}', ']']
+        stack = list()
+        
+        for ch in s:
+            if not stack:
+                stack.append(ch)
+            else:
+                if ch in left:
+                    stack.append(ch)
+                else:
+                    top = stack.pop()
+                    if top in right or left.index(top) != right.index(ch):
+                        return False
+        return not stack
+        
