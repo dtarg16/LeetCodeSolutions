af4fbd8fb905234f46daa0df63387a8edf070b39
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-02-07 14:40:33 +0400

    Time: 44 ms (51.20%), Space: 13.8 MB (96.95%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,15 +1,8 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        
-        
-        freq = {}
-        
+
+        res = ord(t[-1])
         for i in range(len(s)):
-            freq[s[i]]= freq.get(s[i], 0) + 1
-            freq[t[i]]= freq.get(t[i], 0) - 1
-        
-        freq[t[-1]]= freq.get(t[-1], 0) - 1
-        
-        for key, val in freq.items():
-            if val == -1:
-                return key
+            res ^= ord(s[i]) ^ ord(t[i])
+
+        return chr(res)
