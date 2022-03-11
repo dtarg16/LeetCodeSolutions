004c5eaa75379ad35ff496c7f2de065b8e4d0512
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-03-11 12:14:36 +0400

    Time: 48 ms (65.44%), Space: 14.1 MB (32.21%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.py
@@ -0,0 +1,61 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+def reverse_range(head, left, right):
+    prev = None
+    current = head
+    index = 0
+    while index < left:
+        prev = current
+        current = current.next
+        index += 1
+
+    left = prev
+    tail = current
+    while current and index <= right:
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+        index += 1
+    tail.next = current
+    if left:
+        left.next = prev
+    else:
+        head = prev
+    return head
+
+
+def length(head):
+    ans = 0
+    while head:
+        ans += 1
+        head = head.next
+    return ans
+
+class Solution:
+    
+
+    
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        #         head = "------>--->" k =3
+        #         result = "--->----->"
+
+        #         reverse "------>--->"   :   "<---<------"
+        #         reverse "<---"          :   "---><------"
+        #         reverse "<------"       :   "--->------>"
+    
+        if not head or not head.next:
+            return head
+        n = length(head)
+        k = k % n
+        if n == 0 or k == 0 or n == k:
+            return head
+        head = reverse_range(head, 0, n - 1)
+        head = reverse_range(head, 0, k - 1)
+        head = reverse_range(head, k, n - 1)
+
+        return head
