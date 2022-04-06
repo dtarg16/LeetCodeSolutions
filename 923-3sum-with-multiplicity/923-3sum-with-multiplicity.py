981908849f82120354b7655c349027742ed98140
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-04-06 10:52:04 +0400

    Time: 7625 ms (6.39%), Space: 14 MB (92.44%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
@@ -0,0 +1,15 @@
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        
+        dic = {}
+        
+        res = 0;
+        mod = 1000000007;
+        for i in range(len(arr)):
+            res = (res + dic.get(target - arr[i], 0)) % mod
+            
+            for j in range(i):
+                tmp = arr[i] + arr[j]
+                dic[tmp] = dic.get(tmp, 0) + 1
+                
+        return res
