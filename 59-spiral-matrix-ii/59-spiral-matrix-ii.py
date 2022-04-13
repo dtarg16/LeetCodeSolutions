9a8d1d794e693c2726e8684c6a3539229ff231d5
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-04-13 14:21:29 +0400

    Time: 40 ms (68.67%), Space: 13.8 MB (85.84%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -2,21 +2,22 @@ class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         matrix = [[0] * n for _ in range(n)]
                 
-        
-        c = 1
-        mx, nx = 0, n
-        while mx <= nx:
+        def perimeter(mx, nx):
+            tmp = []
             for j in range(mx, nx):
-                matrix[mx][j] = c
-                c += 1
+                tmp.append((mx, j))
             for i in range(mx + 1, nx):
-                matrix[i][nx-1] = c
-                c += 1
+                tmp.append((i, nx - 1))
             for j in reversed(range(mx, nx - 1)):
-                matrix[nx-1][j] = c
-                c += 1
+                tmp.append((nx - 1, j))
             for i in reversed(range(mx + 1, nx - 1)):
-                matrix[i][mx] = c
+                tmp.append((i, mx))
+            return tmp
+            
+        c, mx, nx = 1, 0, n
+        while mx <= nx:
+            for i, j in perimeter(mx, nx):
+                matrix[i][j] = c
                 c += 1
             mx += 1
             nx -= 1
