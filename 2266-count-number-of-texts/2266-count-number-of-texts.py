459047a6131321675c215794b1ddd96f234e4bce
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-05-08 18:04:51 +0400

    Time: 1371 ms (33.33%), Space: 242 MB (16.67%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.py
+++ 2266-count-number-of-texts/2266-count-number-of-texts.py
@@ -1,14 +1,25 @@
 class Solution:
     def countTexts(self, pressedKeys: str) -> int:
-        MOD=pow(10,9)+7
-        dp=[0]*(len(pressedKeys)+1)
-        dp[0]=1
-        for i in range(1,len(pressedKeys)+1):
-            dp[i]=dp[i-1]%MOD
-            if(i-2>=0 and pressedKeys[i-1]==pressedKeys[i-2]):
-                dp[i]=(dp[i]+dp[i-2])%MOD
-                if(i-3>=0 and pressedKeys[i-1]==pressedKeys[i-3]):
-                    dp[i]=(dp[i]+dp[i-3])%MOD
-                    if(pressedKeys[i-1] in "79" and i-4>=0 and pressedKeys[i-1]==pressedKeys[i-4]):
-                        dp[i]=(dp[i]+dp[i-4])%MOD
-        return dp[-1]
+        mp = set(["2", "22", "222",
+                  "3", "33", "333",
+                  '4', '44', '444',
+                  '5', '55', '555',
+                  '6', '66', '666',
+                  '7', '77', '777', '7777', 
+                  '8', '88', '888', 
+                  '9', '99', '999', '9999'])
+        MOD = pow(10, 9) + 7
+        n = len(pressedKeys)
+        
+        @cache
+        def dp(i):
+            if i>=n:
+                return 1
+            ans = 0
+            if i+1<=n and pressedKeys[i:i+1] in mp: ans += dp(i+1)%MOD
+            if i+2<=n and pressedKeys[i:i+2] in mp: ans += dp(i+2)%MOD
+            if i+3<=n and pressedKeys[i:i+3] in mp: ans += dp(i+3)%MOD
+            if i+4<=n and pressedKeys[i:i+4] in mp: ans += dp(i+4)%MOD
+            return ans%MOD
+        
+        return dp(0)%MOD
