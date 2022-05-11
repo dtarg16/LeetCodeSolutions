d6727b526a802112267eb54d8bf9386feee1168a
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-05-11 14:24:27 +0400

    Time: 28 ms (95.81%), Space: 13.8 MB (96.44%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,18 +1,8 @@
+from math import factorial as fact
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        self.d = {1: 'a', 2:'e', 3:'i', 4:'o', 5:'u'}
-        self.n = n
-        self.res = 0
+        return fact(n+4) // (fact(4) * fact(n))
         
-        def rec(sofar, last_index):
-            if len(sofar) == n:
-                self.res += 1
-                return
             
-            for i in range(last_index, 6):
-                rec(sofar + self.d.get(i), i)
-                
-        rec("", 1)
-        
-        return self.res
+            
         
