b3180fab3ac7beaf203d4f57a4656a5a81cb7044
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-05-23 12:03:57 +0400

    Time: 3148 ms (81.84%), Space: 199.5 MB (16.90%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.py
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -3,8 +3,8 @@ class Solution:
         counts = [[s.count("0"), s.count("1")] for s in strs]
 
         @lru_cache(None)
-        def dp(mm, nn, index):
-            if mm < 0 or nn < 0: 
+        def dp(m_rem, n_rem, index):
+            if m_rem < 0 or n_rem < 0: 
                 return -float('inf')
             
             if index == len(strs): 
@@ -12,8 +12,8 @@ class Solution:
             
             zeros, ones = counts[index]
             
-            x = dp(mm-zeros, nn-ones, index + 1)
-            y = dp(mm, nn, index + 1)
+            x = dp(m_rem-zeros, n_rem-ones, index + 1)
+            y = dp(m_rem, n_rem, index + 1)
             
             return max(x + 1, y)
         
