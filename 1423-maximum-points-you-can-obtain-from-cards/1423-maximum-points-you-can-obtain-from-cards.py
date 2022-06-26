f781622c35005ea86eb428b11fced8a504bc05ee
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-06-26 12:36:48 +0400

    Time: 686 ms (29.63%), Space: 27.5 MB (17.27%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -4,6 +4,7 @@ class Solution:
         s = sum(cardPoints[:k])
         res = s
         for i in range(1, k+1):
+            # <------(k-i)---k---------(-k)------(-i)--->
             s += cardPoints[-i] - cardPoints[k-i]
             res = max(res, s)
         return res
