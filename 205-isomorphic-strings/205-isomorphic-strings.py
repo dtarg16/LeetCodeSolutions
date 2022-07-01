61b2d8035b76960a61c0097d1d47e6d33531b0af
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-07-01 11:10:12 +0400

    Time: 85 ms (20.03%), Space: 14.2 MB (44.32%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t = {}
+        t_to_s = {}
+        for i in range(len(s)):
+            if s_to_t.get(s[i], t[i]) != t[i] or t_to_s.get(t[i], s[i]) != s[i]:
+                return False
+            
+            s_to_t[s[i]] = t[i]
+            t_to_s[t[i]] = s[i]
+
+        return True
