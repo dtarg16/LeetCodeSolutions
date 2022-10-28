504a1cb25319a6564d9d33ca08c45c72afa15f0b
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2022-10-28 12:33:19 +0400

    Time: 272 ms (20.44%), Space: 28.6 MB (9.93%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for s in strs:
+            groups[frozenset(Counter(s).items())].append(s)
+        
+        return groups.values()
