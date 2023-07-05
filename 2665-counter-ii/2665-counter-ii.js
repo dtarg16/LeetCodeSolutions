ff4d8e83af947b3c8fea6d1500ec357238fd11be
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2023-07-05 17:59:26 +0400

    Time: 75 ms (20.29%), Space: 45.1 MB (16.16%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+  let presentCount = init;
+
+  function increment() {
+    return ++presentCount;
+  }
+
+  function decrement() {
+      return --presentCount;
+  }
+
+  function reset() {
+      presentCount = init;
+      return presentCount
+  }
+
+  return { increment, decrement, reset };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
