9610dea5bad1bcc30b33b1eca45db9a36f6c916c
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2023-07-06 23:27:43 +0400

    Time: 72 ms (58.81%), Space: 43.4 MB (45.15%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -3,16 +3,7 @@
  * @return {Function}
  */
 var compose = function(functions) {
-  if (functions.length === 0) {
-    return function(x) { return x; };
-  }
-
-  return functions.reduceRight(function(prevFn, nextFn) {
-    return function(x) {
-      return nextFn(prevFn(x));
-    };
-  });
-
+  return x => functions.reduceRight((accumulated, fn) =>fn(accumulated), x)
 };
 
 /**
