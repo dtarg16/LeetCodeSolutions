3ac6ee8a54ca907d5bea351a81b14a526ff89454
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2023-09-02 23:24:33 +0400

    Time: 255 ms (67.46%), Space: 17 MB (29.89%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.py
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.py
@@ -1,15 +1,30 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        max_val = len(s) + 1
-        dp = [max_val] * (len(s) + 1)
-        dp[0] = 0 
-        dictionary_set = set(dictionary)
+        cache = {}
 
-        for i in range(1, len(s) + 1):
-            dp[i] = dp[i - 1] + 1
+        def dfs(text):
+            if len(text) <= 0:
+                return 0
 
-            for l in range(1, i + 1): 
-                if s[i-l:i] in dictionary_set:
-                    dp[i] = min(dp[i], dp[i-l])
-                    
-        return dp[-1]
+            if len(text) in cache:
+                return cache[len(text)]
+            
+            minimum = len(text)
+
+            for word in dictionary:
+                if text == word:
+                    minimum = 0
+                    cache[len(text)] = minimum
+                    return minimum
+
+                elif text[:len(word)] == word:
+                    result = dfs(text[len(word):])
+                    minimum = min(minimum, result)
+
+            minimum = min(minimum, 1 + dfs(text[1:]))
+
+            cache[len(text)] = minimum
+
+            return minimum
+
+        return dfs(s)
