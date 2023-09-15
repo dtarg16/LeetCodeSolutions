92f9154d31bc5f103d16e1d8205a080f56270c2b
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2023-09-15 11:54:51 +0400

    Time: 574 ms (98.25%), Space: 16.8 MB (98.13%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -2,32 +2,24 @@ class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         def manhattan_distance(p1: List[int], p2: List[int]) -> int:
             return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+        
+        points, distance, weight = [tuple(point) for point in points], {}, 0
+        
+        for point in points:
+            distance[point] = float('inf')
 
-        n = len(points)
-        visited = [False] * n
-        heap_dict = {0: 0}  
-        min_heap = [(0, 0)]
+        distance[points[0]] = 0
         
-        mst_weight = 0
+        while distance:
+            point = min(distance, key=distance.get)
+           
+            weight += distance.pop(point)
+            
+            for next_point in distance:
+                distance[next_point] = min(distance[next_point], manhattan_distance(point, next_point))
         
-        while min_heap:
-            weight, point = heappop(min_heap)
-            
-            if visited[point] or heap_dict.get(point, float('inf')) < weight:
-                continue
-            
-            visited[point] = True
-            mst_weight += weight
-            
-            for next_point in range(n):
-                if visited[next_point]:
-                    continue
-                
-                new_distance = manhattan_distance(points[point], points[next_point])
-    
-                if new_distance < heap_dict.get(next_point, float('inf')):
-                    heap_dict[next_point] = new_distance
-                    heappush(min_heap, (new_distance, next_point))
-        
-        return mst_weight
+        return weight
 
+
+
+        
