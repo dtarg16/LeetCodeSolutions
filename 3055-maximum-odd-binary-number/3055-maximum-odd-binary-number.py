bafef1262b57ebb6c965d507686de0b33e650d23
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2023-09-27 00:14:17 +0400

    Time: 46 ms (26.37%), Space: 16.2 MB (77.32%) - LeetHub

--- 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
@@ -1,13 +1,4 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        counter = collections.Counter(list(s))
-        res = []
-        for _ in range(counter["1"] - 1):
-            res.append("1")
-
-        for _ in range(counter["0"]):
-            res.append("0")
-
-        res.append("1")
-
-        return "".join(res)
+        counter = collections.Counter(s)
+        return '1'*(counter['1']-1) + '0'*counter['0'] + '1'
