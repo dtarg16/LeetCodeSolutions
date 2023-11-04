93da6ce46719c5df979201869a3c92f06197a97f
David Targamadze <56018105+dtarg16@users.noreply.github.com>
2023-11-05 00:11:10 +0400

    Time: 16 ms (41.37%), Space: 43.6 MB (48.29%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0, right = s.length() - 1;
+        while(left <= right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left += 1;
+            right -= 1;
+        }
+        return true;
+    }
+}
