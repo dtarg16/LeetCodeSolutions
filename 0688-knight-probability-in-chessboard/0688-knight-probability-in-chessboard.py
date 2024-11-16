5302fc5fd802a30a56fa33fd639236d2bab5a8f9
David Targamadze <56018105+Va1RuS@users.noreply.github.com>
2024-11-16 11:43:40 +0400

    Time: 150 ms (49.26%), Space: 25.8 MB (26.2%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -2,18 +2,16 @@ class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
         moves = [(i, j) for i in range(-2, 3) for j in range(-2, 3) if abs(i) != abs(j) and i*j != 0 ]
 
-        visited = {}
 
-        def dp(row, col, n, k):
-            if k<0:
-                return 1
-            elif (row < 0 or row >= n) or (col < 0 or col >= n):
+        @cache
+        def dp(row, col, k):
+            if not(0 <= row < n and 0 <= col < n):
                 return 0
-            elif (row, col, k) in visited:
-                return visited[(row, col, k)]
+            
+            if k==0:
+                return 1
 
-            prob = sum(dp(row + i, col + j, n, k - 1) * 1/8 for i, j in moves)
-            visited[(row, col, k)] = prob
-            return prob
+            return sum(dp(row + i, col + j, k - 1) * 1/8 for i, j in moves)
 
-        return dp(row, column, n, k)
+
+        return dp(row, column, k)
